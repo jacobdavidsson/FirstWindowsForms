73fbf43a0d307889ac4dcc1120435b71d61e3fe0
Jacob <jacobdavidsson@live.com>
2023-05-10 17:28:00 +0200

    Add project files.

---
+++ FirstWindowsForms/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace FirstWindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.TxtAge = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(352, 274);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(164, 54);
+            this.button.TabIndex = 0;
+            this.button.Text = "Spara i Databas";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.Button_insert);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(347, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ålder";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(347, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Namn";
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(478, 143);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(156, 26);
+            this.TxtName.TabIndex = 4;
+            // 
+            // TxtAge
+            // 
+            this.TxtAge.Location = new System.Drawing.Point(478, 202);
+            this.TxtAge.Name = "TxtAge";
+            this.TxtAge.Size = new System.Drawing.Size(156, 26);
+            this.TxtAge.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(352, 334);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 54);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Hämta Data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button_fetch);
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(361, 431);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(39, 32);
+            this.lblID.TabIndex = 6;
+            this.lblID.Text = "id";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.Location = new System.Drawing.Point(636, 431);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(68, 32);
+            this.lblAge.TabIndex = 6;
+            this.lblAge.Text = "Age";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(477, 431);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(93, 32);
+            this.lblName.TabIndex = 6;
+            this.lblName.Text = "Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(478, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(347, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(568, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 53);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button_delete);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(568, 341);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 47);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Updatera data";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button_update);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1191, 779);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.TxtAge);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.TextBox TxtAge;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ FirstWindowsForms/Form1.cs
@@ -0,0 +1,137 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstWindowsForms
+{
+    public partial class Form1 : Form
+    {
+
+        MySqlConnection conn;
+
+        // Test push 
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string server = "localhost";
+            string database = "elever";
+            string user = "root";            
+            string pass = "EmmaWindows";  
+
+           string connString  = $"SERVER={server}; DATABASE = {database}; UID={user}; PASSWORD={pass};";
+
+            conn = new MySqlConnection(connString); 
+        }
+
+
+
+        // insert query
+        private void Button_insert(object sender, EventArgs e)
+        {
+            // hämta text från textfält
+            string strName = TxtName.Text;
+            int intAge = Convert.ToInt32(TxtAge.Text);
+
+            // skriv SQL insert statment  "c
+            string strSql = $"INSERT INTO elever1(elever1_name, elever1_age) VALUES ('{ strName}', {intAge})";
+
+            // skapa en mysqlcommand objekt
+            MySqlCommand cmd = new MySqlCommand(strSql, conn);
+
+            // öppna koppling till DB
+            conn.Open();
+
+            // skicka iväg SQL command till db
+            cmd.ExecuteReader();
+
+            // stänga koppling till db
+            conn.Close();
+
+            // bekräftelse till användare "insert completed" 
+            MessageBox.Show("Data insert completed"); // pop-up box
+        }
+
+
+        // hämta data query
+        private void Button_fetch(object sender, EventArgs e)
+        {
+            // skriva sql statment
+            string strsql =  $"SELECT * FROM elever1";
+
+            // skapa mysql command objekt
+            MySqlCommand cmd = new MySqlCommand(strsql, conn);
+
+            // nollställer objekt
+            lblID.Text = "ID";
+            lblName.Text = "Name";
+            lblAge.Text = "Age";
+
+            // öppnar koppling till db igen
+            conn.Open();
+
+            // exekvera kommando till db
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            // använder while loop för att läsa varje rad 
+
+            while (reader.Read())
+            {
+                // så länge det finns rader att läsa är vi i while loopen
+
+                // skriver ut id värde
+                lblID.Text += Environment.NewLine + reader["elever1_id"];
+                lblName.Text += Environment.NewLine + reader["elever1_name"];
+                lblAge.Text += Environment.NewLine + reader["elever1_age"];
+
+                
+
+            }
+            // stäng koppling
+
+            conn.Close();           
+
+        }
+
+
+        // delete query
+        private void Button_delete(object sender, EventArgs e)
+        {
+            string strId = textBox1.Text;
+            string sql = $"DELETE FROM elever1 WHERE elever1_id = ({strId})";         
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            
+
+            conn.Open();
+            MySqlDataReader reader = cmd.ExecuteReader();
+            conn.Close();
+            MessageBox.Show("Delete of student completed.");
+        }
+
+
+        // update query
+        private void Button_update(object sender, EventArgs e)
+        {
+            string strId = textBox1.Text;
+            string strName = TxtName.Text;
+            int intAge = Convert.ToInt32(TxtAge.Text);
+
+            string strSql = $"UPDATE elever1 SET elever1_name = '{strName}', elever1_age = {intAge} WHERE elever1_id = ({strId})";
+
+            MySqlCommand cmd = new MySqlCommand(strSql, conn);
+
+            conn.Open();
+            cmd.ExecuteReader();
+            conn.Close();
+            MessageBox.Show("Update completed"); // pop-up box
+        }
+    }
+}
